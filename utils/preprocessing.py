7e5f9b55bea831df135e205bccf86ec60b64a1b4
Lucas Puerari <lucas.puerari@unimib.it>
2021-11-22 13:58:37 +0100

    Add snr normalization, Update default normalization

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -19,7 +19,7 @@ def preprocessing(image, image_dim, normalization_type):
     def normalization_fn(type):
 
         def default(image):
-            return tf.image.per_image_standardization(image)
+            return tf.math.divide(image, 255)
 
         def multichannel(image):
             def ch_normalization(channel):
@@ -37,11 +37,42 @@ def preprocessing(image, image_dim, normalization_type):
             image = tf.stack(channels, axis=2)
             return image
 
+        def snr(image):
+            def ch_normalization(channel):
+                channel = channel.numpy()
+
+                mask_low_ids = channel <= 1e-7
+                mask_up_ids = channel > 1.0
+
+                min = channel[np.logical_not(mask_low_ids)].min()
+                max = channel[np.logical_not(mask_up_ids)].max()
+
+                mask_ids = np.logical_or(
+                    np.logical_not(mask_low_ids),
+                    np.logical_not(mask_up_ids)
+                )
+
+                if (max - min) > 0:
+                    channel[mask_ids] = (channel[mask_ids] - min) / (max - min)
+
+                channel[mask_low_ids] = 0.0
+                channel[mask_up_ids] = 1.0
+
+                return tf.convert_to_tensor(channel)
+
+            channels = tf.unstack(image, axis=2)
+            channels = [ch_normalization(channel) for channel in channels]
+            image = tf.stack(channels, axis=2)
+            return image
+
         if type == default.__name__:
             return default
 
         elif type == multichannel.__name__:
             return multichannel
+
+        elif type == snr.__name__:
+            return snr
 
     processes = [
         replace_nan_fn,
